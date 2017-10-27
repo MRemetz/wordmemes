af336b577a55b579aff62576bd5e2cf0266713e6
Connor Zapfel <connorzapfel@gmail.com>
2017-10-27 19:15:14 -0400

    Updated the word-box ID

--- static/js/main.js
+++ static/js/main.js
@@ -5,7 +5,7 @@ function updateWord(){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
-            document.getElementById("wordbox").innerHTML = this.responseText;
+            document.getElementById("word-box").innerHTML = this.responseText;
         }
     };
     xhttp.open("GET","http://127.0.0.1:5000/get_new_word",true);
