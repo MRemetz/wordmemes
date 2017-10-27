cb903f4bfe61e9d32e8b0fb7d258ec3d3e9546fe
Max Remetz <max@remetz.com>
2017-10-27 19:53:56 -0400

    Im in love with stacy's mom

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("./index.html")
+
+def get_new_word():
+    with open("wordlist.txt", "r") as words:
+        num = random.randint(0, 1719)
+        for i, line in enumerate(words):
+            print(i)
+            if i == num:
+                line = str(line).rstrip();
+                return line
+
+print(get_new_word())
