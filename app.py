b2d21bc261ef0dc5e64a61e5a045aaaa8d90c313
Connor Zapfel <connorzapfel@gmail.com>
2017-10-28 00:54:18 -0400

    Wordlist is now dynamically sized

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import random, socket
 
 app = Flask(__name__)
 ip = socket.gethostbyname(socket.gethostname())
+word_count = 0;
+
+with open("wordlist.txt","r") as f:
+    for x in f:
+        word_count += 1
 
 @app.route("/")
 def index():
@@ -11,8 +16,7 @@ def index():
 @app.route("/get_new_word")
 def get_new_word():
     with open("wordlist.txt", "r") as words:
-        num = random.randint(0, 1719)
+        num = random.randint(0, word_count)
         for i, line in enumerate(words):
             if i == num:
                 return str(line).rstrip();
-
