7feb5a74012c85336193a9371f7348afae46fdf0
Connor Zapfel <connorzapfel@gmail.com>
2017-10-27 20:25:30 -0400

    Added code to grab the host IP

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
-import random
+import random, socket
 
 app = Flask(__name__)
+ip = socket.gethostbyname(socket.gethostname())
 
 @app.route("/")
 def index():
-    return render_template("./index.html")
+    return render_template("./index.html",ip_addr=ip)
 
 @app.route("/get_new_word")
 def get_new_word():
@@ -14,3 +15,4 @@ def get_new_word():
         for i, line in enumerate(words):
             if i == num:
                 return str(line).rstrip();
+
