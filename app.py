bc85f86139e949a94925bf0bbb3210c2a60b1b65
Connor Zapfel <connorzapfel@gmail.com>
2018-02-25 21:47:42 -0500

    Should get better performance now when selecting words; Code is less complicated; Added light comments

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 import random, socket
 
 
+# Create the list of words to use
+words = []
+
+# Get the IP that the server should embed in the page
 def getIp():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
@@ -9,31 +13,28 @@ def getIp():
     s.close() 
     return ip
 
+# Create a fresh, randomized list of words
+def generate_word_list():
+    with open("wordlist.txt","r") as f:
+        for x in f:
+            words.append(x)
+    random.shuffle(words)
+
+# Create the actual Flask app
 app = Flask(__name__)
-print(f"Starting server on: {getIp()}")
-word_count = 0;
-used_words =  []
+# Populate words with the initial list
+generate_word_list()
 
-with open("wordlist.txt","r") as f:
-    for x in f:
-        word_count += 1
-
+# The main route that contains all the user interactable content
 @app.route("/")
 def index():
     return render_template("./index.html",ip_addr=getIp())
 
+# The route for grabbing the next word
 @app.route("/get_new_word")
 def get_new_word():
-    with open("wordlist.txt", "r") as words:
-        num = random.randint(0, word_count)
-        for i, line in enumerate(words):
-            if line in used_words:
-                num += 1
-            if i == num:
-                used_words.append(i)
-                return str(line).rstrip();
-
-@app.route("/clearlist")
-def clearlist():
-    used_words = []
-    return ""
+    if len(words) == 0:
+        generate_word_list()
+    w = str(words[0])
+    del words[0]
+    return w
